4339498b8abc48a90a597a120b7f6effaae70c53
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2022-12-12 15:24:29 -0300

    Criação do Cloud Gateway
    Executando LoadBalance

--- clienteMS/src/main/java/application/domain/controller/ClienteController.java
+++ clienteMS/src/main/java/application/domain/controller/ClienteController.java
@@ -2,6 +2,7 @@ package application.domain.controller;
 
 import application.domain.dto.ClienteDTO;
 import application.domain.service.ClienteService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,11 +12,16 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("clientes")
+@Slf4j
 public class ClienteController {
 
     @Autowired
     private ClienteService service;
 
+    @GetMapping
+    public String ok(){
+        return "Ok";
+    }
 
     @PostMapping
     public ResponseEntity save(@RequestBody ClienteDTO dto) {
