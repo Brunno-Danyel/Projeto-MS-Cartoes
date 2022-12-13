e95b76421e58f1508fa53980cb45ba984e349cdf
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2022-12-13 11:11:47 -0300

    Criando o MS de cartões

---
+++ mscartoes/src/main/java/Application/CartoesApplication.java
@@ -0,0 +1,14 @@
+package Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CartoesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartoesApplication.class, args);
+    }
+}
