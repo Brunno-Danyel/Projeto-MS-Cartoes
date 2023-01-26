7b3c755997482fd8a22856b8ae0914263567d8e8
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2023-01-26 14:35:39 -0300

    Criando e configurando o MS de Avaliador de credito

---
+++ msavaliadorCredito/src/main/java/application/controller/AvaliadorCreditoController.java
@@ -0,0 +1,19 @@
+package application.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("avaliacoes-credito")
+public class AvaliadorCreditoController {
+
+    @GetMapping
+    public String status(){
+        return "ok";
+    }
+
+
+
+
+}
