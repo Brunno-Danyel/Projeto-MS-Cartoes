82c5d123573890db28d6381b0ec21854e517736f
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2023-02-04 13:16:23 -0300

    >> Ajustes <<
    Anotação @Bean adicionada ao respectivo método

--- msavaliadorCredito/src/main/java/application/config/MQConfig.java
+++ msavaliadorCredito/src/main/java/application/config/MQConfig.java
@@ -2,15 +2,17 @@ package application.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
 public class MQConfig {
 
-    @Value("${mq.queues.emissao-cartoes}")
+    @Value("${mq.queue.emissao-cartoes}")
     private String emissaoCartoesFila;
 
+    @Bean
     public Queue queueEmissaoCartoes() {
         return new Queue(emissaoCartoesFila, true);
     }
